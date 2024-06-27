54aeb35e457fc3a55f12a4b2f009795b120398f9
maxnannij <147406754+maxnannij@users.noreply.github.com>
2024-06-27 15:19:41 -0300

    Update tabs.js

--- tabs.js
+++ tabs.js
@@ -4,13 +4,19 @@ document.getElementById('tab1').addEventListener('click', function() {
 document.getElementById('tab2').addEventListener('click', function() {
     switchTab('section2', 'tab2');
 });
+document.getElementById('tab3').addEventListener('click', function() {
+    switchTab('section3', 'tab3');
+});
 
 function switchTab(sectionId, tabId) {
     document.getElementById('section1').classList.add('hidden');
     document.getElementById('section2').classList.add('hidden');
-    document.getElementById(sectionId).classList.remove('hidden');
+   document.getElementById('section3').classList.add('hidden'); 
+  document.getElementById(sectionId).classList.remove('hidden');
 
     document.getElementById('tab1').classList.remove('active');
-    document.getElementById('tab2').classList.remove('active');
+    document.getElementById('tab2').classList.remove('active');  
+  document.getElementById('tab3').classList.remove('active');
     document.getElementById(tabId).classList.add('active');
 }
+
