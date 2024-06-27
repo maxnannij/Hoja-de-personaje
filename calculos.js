852e9631a439f21b8ecda2900adbfee871b2eb92
maxnannij <147406754+maxnannij@users.noreply.github.com>
2024-06-27 15:05:21 -0300

    Update and rename scripts.js to calculos.js

--- calculos.js
+++ calculos.js
@@ -1,20 +1,3 @@
-document.getElementById('tab1').addEventListener('click', function() {
-    switchTab('section1', 'tab1');
-});
-document.getElementById('tab2').addEventListener('click', function() {
-    switchTab('section2', 'tab2');
-});
-
-function switchTab(sectionId, tabId) {
-    document.getElementById('section1').classList.add('hidden');
-    document.getElementById('section2').classList.add('hidden');
-    document.getElementById(sectionId).classList.remove('hidden');
-
-    document.getElementById('tab1').classList.remove('active');
-    document.getElementById('tab2').classList.remove('active');
-    document.getElementById(tabId).classList.add('active');
-}
-
 function calcularModificador(valor) {
     return Math.max(Math.floor((valor - 10) / 2), 0);
 }
